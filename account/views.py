78ef2f75b0b1a2618951c96e78e4f58e9fec8f5d
venu <venu082001@gmail.com>
2025-04-14 15:22:56 +0530

    made urls file for account

--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 # Create your views here.
+
+
+class Test(APIView):
+    def get(self, request):
+        return Response({"message": "Working"}, status=status.HTTP_200_OK)
