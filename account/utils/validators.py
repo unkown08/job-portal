657704241a5d1572a50a8d2341c9e0c770550327
venu <venu082001@gmail.com>
2025-04-16 16:15:49 +0530

    fixed mistake in util validator

--- account/utils/validators.py
+++ account/utils/validators.py
@@ -6,15 +6,15 @@ def password_validation(value: str):
             raise serializers.ValidationError(
                 "Password must be at least 5 characters long" 
             )
-        if not re.search(r'[^a-zA-Z0-9]', value):
-            raise serializers.ValidationError(
-                "Password must have at least one special character" 
-            )
-        if not re.search(r'[A-Z]', value):
-            raise serializers.ValidationError(
-                "Password must have at least one upper case character"
-            )
-        if not re.search(r'[0-9]', value):
-            raise serializers.ValidationError(
-                "Password must have at least one number character"
-            )
+    if not re.search(r'[^a-zA-Z0-9]', value):
+        raise serializers.ValidationError(
+            "Password must have at least one special character" 
+        )
+    if not re.search(r'[A-Z]', value):
+        raise serializers.ValidationError(
+            "Password must have at least one upper case character"
+        )
+    if not re.search(r'[0-9]', value):
+        raise serializers.ValidationError(
+            "Password must have at least one number character"
+        )
