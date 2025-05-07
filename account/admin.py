ae7bfde37f6cd2f29e68e5e3eb879df5f8b9ddb5
venu <venu082001@gmail.com>
2025-05-07 15:22:41 +0530

    registered recruiter model

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import CustomUser
 from .custom_models.job_seeker_models import Education, UserLink, Experience, JobSeeker
+from .custom_models.recruiter_models import Recruiter
 # Register your models here.
 
 admin.site.register(CustomUser)
@@ -8,3 +9,4 @@ admin.site.register(Education)
 admin.site.register(Experience)
 admin.site.register(UserLink)
 admin.site.register(JobSeeker)
+admin.site.register(Recruiter)
--- account/models.py
+++ account/models.py
@@ -12,4 +12,3 @@ class CustomUser(AbstractUser):
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='job_seeker')
     def __str__(self):
         return self.username 
- 
